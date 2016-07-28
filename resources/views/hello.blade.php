536730fa3e8b81a054804ff1740e9850e0cf8283
LuoTurtle <turtle192425@gmail.com>
2016-07-28 10:34:31 +0800

    套自我介紹版

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE HTML>
+<!--
+	Epilogue by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>個人檔案</title>
+
+            <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="inner">
+					<h1>You want know Me!?</h1>
+					<p>這是我套用網路參考樣板，搭配Eclise所製作出的簡單個人介紹網站</p>
+				</div>
+			</header>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Banner -->
+					<section id="intro" class="main">
+						<h2>I'm 南投Boy</h2>
+						<p>從小生長在風光明媚的南投，國小國中因其與世無爭的環境，所以我並無遇到什麼學業上的對手，成績皆名列前茅，也這樣順利地考上了其南投第一志願-中興高中；但因從小沒什麼競爭性，出來大城市後便遭遇到了許多挫折，但我並未因此而喪失鬥志，反而奮發向上，並朝著自己所感興趣的方向一步步努力前進著。前身為元智大學資訊工程學系，因無法適應其電子與硬體部分而轉學至了逢甲大學現在這個學系，但還是非常喜歡撰寫程式碼的感覺，自認邏輯思維不錯，所以雖然離開了資訊工程，但還是有在自我進修各種程式撰寫方式。我的生活哲學是不管遇到什麼困難，都要笑笑地去面對；這是因為在我很小的時候，曾聽到我爺爺這麼對我說:『你現在看到的陰影，是因為你背對著陽光，你只要轉過身來，就又會是個光芒四射的景色。』我受此句話的影響非常深遠，我也將此句話時時刻刻放在我心中，期許我能夠時時刻刻保持微笑；不只是面對陽光，我更要成為我親友間的小太陽。</p>
+					</section>
+
+				<!-- Items -->
+					<section class="main items">
+						<article class="item">
+							<header>
+								<a href="http://www.up.fcu.edu.tw/wSite/mp?mp=335101"><img src="images/head.jpg" alt="" /></a>
+								<h3>畢業科系</h3>
+							</header>
+							<br><p>逢甲大學 都市計畫與空間資訊<br>畢業年分:2016年6月</p><br>
+						</article>
+						<article class="item">
+							<header>
+								<a href="#"><img src="images/pic02.jpg" alt="" /></a>
+								<h3>關於我</h3>
+							</header>
+							<p><br><br>想更多了解我嗎?<br><br></p>
+						</article>
+						<article class="item">
+							<header>
+								<a href="https://www.gis.tw/"><img src="images/pic03.jpg" alt="" /></a>
+								<h3>實習經歷</h3>
+							</header>
+							<p>實習生／逢甲GIS中心 ( 2015/06～ 2015/09)<br>職務類別：資訊助理人員<br>產業類別：其它軟體及網路相關業<br>實習內容：GIS資料相關處理協助、資料庫相關學習</p>
+						</article>
+						<article class="item">
+							<header>
+                                                            <a href="#"><img src="{{asset('images/4.jpg')}}"alt=""/></a>
+								<h3>專題經驗</h3>
+							</header>
+							<p>系統研究-跟車系統app開發<br>專題研究-水庫上游集水區土地使用與水質相關性<br>專題研究-筏子溪周邊地區都市計畫開發模擬<br>專題研究-台中市南區南和、永興、永和里都市環境通盤檢討計畫
+							</p>
+						</article>
+					</section>
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+							<li><a href="https://www.facebook.com/jiankui.luo" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						</ul>
+						<p >Email:  turtle192425@gmail.com</p>
+					</footer>
+
+			</div>
+
+	</body>
+</html>
